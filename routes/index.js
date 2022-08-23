e17a5d27708c19467ba54416e8cea304ec97efac
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-08-24 00:21:31 +0800

    feat: add sort function

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const sort = require('./modules/sort')
 const users = require('./modules/users')
 
 router.use('/users', users)
 router.use('/restaurants', restaurants)
+router.use('/sort', sort)
 router.use('/', home)
 
 module.exports = router
