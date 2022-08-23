5679b89a10747c59792d6a7ab8ffd16255abaa0b
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-08-24 01:28:10 +0800

    refactor: modify register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -21,9 +21,24 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  const { name, email, password } = req.body
-  return User.create({ name, email, password })
-    .then(() => res.redirect('/users/login'))
+  const { name, email, password, confirmPassword } = req.body
+  return User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('User already exists.')
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        return User.create({ name, email, password })
+          .then(() => res.redirect('/users/login'))
+          .catch(err => console.log(err))
+      }
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
