f26afeea1efc7d3f017d8ac50d433cc00a4374f1
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-08-24 10:36:07 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ app.use(express.static('public'))
 
 usePassport(app)
 
+app.use((req, res, next) => {
+  // console.log(req.user)
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 app.use(routes)
 
 app.listen(port, () => {
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -2,16 +2,6 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
-router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findOne({ _id: id })
-    .lean()
-    .then((restaurants) => {
-      res.render('show', ({ restaurants, id }))
-    })
-    .catch(err => console.log(err))
-})
-
 router.get('/add', (req, res) => {
   res.render('add')
 })
@@ -26,6 +16,16 @@ router.post('/add', (req, res) => {
       console.log(err)
       res.render('add', ({ name, name_en, category, image, location, phone, google_map, rating, description }))
     })
+})
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findOne({ _id: id })
+    .lean()
+    .then((restaurants) => {
+      res.render('show', ({ restaurants, id }))
+    })
+    .catch(err => console.log(err))
 })
 
 router.get('/:id/edit', (req, res) => {
