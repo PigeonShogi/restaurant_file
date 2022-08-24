ee5df9c63fe78ed8d14a26e1b6496c452641656a
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-08-24 14:45:56 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 require('./config/mongoose')
 
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/restaurants', authenticator, restaurants)
 router.use('/search', authenticator, search)
 router.use('/sort', authenticator, sort)
