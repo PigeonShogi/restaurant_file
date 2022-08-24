3ac745f0ef96808131905f7fe52b86c005fc32d1
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-08-25 00:03:18 +0800

    modify todoSeeder

---
+++ models/default_data/seed_user.js
@@ -0,0 +1,16 @@
+const seed_user = [
+  {
+    email: 'user1@example.com',
+    password: '12345678',
+    start: 0,
+    end: 3
+  },
+  {
+    email: 'user2@example.com',
+    password: '12345678',
+    start: 3,
+    end: 6
+  }
+]
+
+module.exports = seed_user
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -7,64 +8,47 @@ const Restaurant = require('../restaurant')
 const User = require('../user')
 const db = require('../../config/mongoose')
 
-const restaurantsJSON = require('../restaurant.json').results
+const restaurantsJSON = require('../default_data/restaurant.json').results
+const SEED_USER = require('../default_data/seed_user')
 
-const SEED_USER = {
-  email: 'user1@example.com',
-  password: 12345678
-}
-const SEED_RESTAURANT = {
-  id: 1,
-  name: "Sababa 沙巴巴中東美食",
-  name_en: "Sababa Pita Bar",
-  category: "中東料理",
-  image: "https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5635/01.jpg",
-  location: "台北市羅斯福路三段 283 巷 17 號",
-  phone: "02 2363 8009",
-  google_map: "https://goo.gl/maps/BJdmLuVdDbw",
-  rating: 4.1,
-  description: "沙巴巴批塔是台灣第一家純手工批塔專賣店,只選用最新鮮的頂級原料,以及道地的中東家傳配方。"
-}
-
-db.once('open', () => {
-  return User.create({
-    email: SEED_USER.email,
-    password: SEED_USER.password
-  })
-    // .then(() => {
-    //   return Promise.all(Array.from(
-    //     restaurantsJSON,
-    //     (value) => {
-    //       Restaurant.create({
-    //         id: value.id,
-    //         name: value.name,
-    //         name_en: value.name_en,
-    //         category: value.category,
-    //         image: value.image,
-    //         location: value.location,
-    //         phone: value.phone,
-    //         google_map: value.google_map,
-    //         rating: value.rating,
-    //         description: value.description,
-    //         // userId
-    //       })
-    //     }))
-    .then(() => {
-      return Restaurant.create({
-        id: SEED_RESTAURANT.id,
-        name: SEED_RESTAURANT.name,
-        name_en: SEED_RESTAURANT.name_en,
-        category: SEED_RESTAURANT.category,
-        image: SEED_RESTAURANT.image,
-        location: SEED_RESTAURANT.location,
-        phone: SEED_RESTAURANT.phone,
-        google_map: SEED_RESTAURANT.google_map,
-        rating: SEED_RESTAURANT.rating,
-        description: SEED_RESTAURANT.description,
+function createSeed(seedArray, arrayIndex, seedJson) {
+  const start = seedArray[arrayIndex].start
+  const end = seedArray[arrayIndex].end
+  const targetRestaurant = seedJson.slice(start, end)
+  db.once('open', () => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(seedArray[arrayIndex].password, salt))
+      .then(hash => User.create({
+        email: seedArray[arrayIndex].email,
+        password: hash
+      }))
+      .then(newUser => {
+        const userId = newUser._id
+        return Promise.all(Array.from(
+          targetRestaurant,
+          value => {
+            Restaurant.create({
+              id: value.id,
+              name: value.name,
+              name_en: value.name_en,
+              category: value.category,
+              image: value.image,
+              location: value.location,
+              phone: value.phone,
+              google_map: value.google_map,
+              rating: value.rating,
+              description: value.description,
+              userId
+            })
+          }
+        ))
       })
-    })
-    .then(() => {
-      console.log('Restaurant Seeder Done!')
-      process.exit()
-    })
-})
+      .then(() => {
+        console.log('Restaurant Seeder Done!')
+      })
+  })
+}
+
+createSeed(SEED_USER, 0, restaurantsJSON)
+createSeed(SEED_USER, 1, restaurantsJSON)
